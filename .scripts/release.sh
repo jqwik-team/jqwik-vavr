1670a491307ec0afd7346d787ae02e1529a6a51b
bosob <bosob@muellern.de>
2020-10-06 09:12:58 +0200

    Update release.sh

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -13,6 +13,11 @@ fi
 release_version=$1
 development_version="$2-SNAPSHOT"
 
+echo "Release notes by git commit message generated (see .release_notes_$release_version.txt"
+git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" -i -E > .release_notes_"$release_version".txt
+#git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" -i -E --grep="^\s*(\[FEATURE\]|\[FIX\])]" > .release_notes_"$release_version".txt
+#git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" -i -E --grep="^\s*(\[INTERNAL\]|\[FEATURE\]|\[FIX\]|\[DOC\])*(\[FEATURE\]|\[FIX\])]" > .release_notes_"$release_version".txt
+
 echo "Start release $release_version by adding tag (see Travis CI)"
 git tag -a $release_version -m "Release $release_version"
 git push origin $release_version
@@ -23,8 +28,5 @@ mvn org.codehaus.mojo:versions-maven-plugin:2.5:commit
 git ls-files --modified | grep 'pom\.xml$' | xargs git add
 git commit -m "Update development version to $development_version"
 
-echo "Release notes by git commit message generated (see .release_notes_$release_version.txt"
-git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" -i -E --grep="^\s*(\[FEATURE\]|\[FIX\])]" > .release_notes_"$release_version".txt
-#git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" -i -E --grep="^\s*(\[INTERNAL\]|\[FEATURE\]|\[FIX\]|\[DOC\])*(\[FEATURE\]|\[FIX\])]" > .release_notes_"$release_version".txt
-
 echo "Please commit and push the change with 'git push origin' and update release notes in GitHub!"
+echo "Please update version on README.md!"

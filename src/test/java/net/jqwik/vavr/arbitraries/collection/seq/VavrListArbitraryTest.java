ba02fd4070e984fa4b9b69de89382e0cf07df9dc
Johannes Link <business@johanneslink.net>
2023-03-27 10:50:21 +0200

    Remove VavrUniqueElementsConfigurator

--- src/test/java/net/jqwik/vavr/arbitraries/collection/seq/VavrListArbitraryTest.java
+++ src/test/java/net/jqwik/vavr/arbitraries/collection/seq/VavrListArbitraryTest.java
@@ -9,8 +9,12 @@ import net.jqwik.api.constraints.UniqueElements;
 import net.jqwik.vavr.api.VavrArbitraries;
 import net.jqwik.vavr.arbitraries.base.VavrTraversableArbitraryTestBase;
 
+import java.util.function.Function;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.not;
 
 class VavrListArbitraryTest extends VavrTraversableArbitraryTestBase<List<Integer>, List<Boolean>> {
 
@@ -25,4 +29,18 @@ class VavrListArbitraryTest extends VavrTraversableArbitraryTestBase<List<Intege
         assertThat(list.distinct().size(), is(list.size()));
     }
 
+    @Property
+    void generateDistinctByEvenOdd(@ForAll @UniqueElements(by = ByEvenOdd.class) final List<Integer> list) {
+        assertThat(list.size(), lessThanOrEqualTo(2));
+        if (list.size() == 2) {
+            assertThat(list.get(0) % 2, not(list.get(1) % 2));
+        }
+    }
+
+    private static class ByEvenOdd implements Function<Integer, Object> {
+        @Override
+        public Boolean apply(Integer o) {
+            return o % 2 == 0;
+        }
+    }
 }
